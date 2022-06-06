8256f88bf7e61778e02272ee745a6b688499ee21
Aditya Saha <aditya.saha2017@gmail.com>
2022-06-06 19:19:04 +0530

    Frontend Routing and Header

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Header />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
